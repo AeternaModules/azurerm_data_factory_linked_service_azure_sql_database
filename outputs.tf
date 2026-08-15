14a2317f6b8f45a894f2229f615988141439ef79
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:02:48 +0100

    Update to schema version v4.81.0

--- outputs.tf
+++ outputs.tf
@@ -32,11 +32,11 @@ output "data_factory_linked_service_azure_sql_databases_integration_runtime_name
 }
 output "data_factory_linked_service_azure_sql_databases_key_vault_connection_string" {
   description = "Map of key_vault_connection_string values across all data_factory_linked_service_azure_sql_databases, keyed the same as var.data_factory_linked_service_azure_sql_databases"
-  value       = { for k, v in azurerm_data_factory_linked_service_azure_sql_database.data_factory_linked_service_azure_sql_databases : k => v.key_vault_connection_string if v.key_vault_connection_string != null && length(v.key_vault_connection_string) > 0 }
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_sql_database.data_factory_linked_service_azure_sql_databases : k => one(v.key_vault_connection_string) if v.key_vault_connection_string != null && length(v.key_vault_connection_string) > 0 }
 }
 output "data_factory_linked_service_azure_sql_databases_key_vault_password" {
   description = "Map of key_vault_password values across all data_factory_linked_service_azure_sql_databases, keyed the same as var.data_factory_linked_service_azure_sql_databases"
-  value       = { for k, v in azurerm_data_factory_linked_service_azure_sql_database.data_factory_linked_service_azure_sql_databases : k => v.key_vault_password if v.key_vault_password != null && length(v.key_vault_password) > 0 }
+  value       = { for k, v in azurerm_data_factory_linked_service_azure_sql_database.data_factory_linked_service_azure_sql_databases : k => one(v.key_vault_password) if v.key_vault_password != null && length(v.key_vault_password) > 0 }
 }
 output "data_factory_linked_service_azure_sql_databases_name" {
   description = "Map of name values across all data_factory_linked_service_azure_sql_databases, keyed the same as var.data_factory_linked_service_azure_sql_databases"
